d908adbc13cb11892106a4d650967de6079e8927
Mark Towers <mark.m.towers@gmail.com>
2021-03-15 13:31:45 +0000

    Changes the metrics to save to a list allowing JSON to save the data

--- src/core/common.py
+++ src/core/common.py
@@ -63,11 +63,12 @@ def get_beam_output_no_normalization(output_file):
 
 
 def model_top_metric_eval(model, validation_lidar_data, validation_beam_output):
-    beam_output_true, _ = get_beam_output_no_normalization('data/beams_output_validation.npz')
-    predictions = np.argsort(model.predict(validation_lidar_data, batch_size=100), axis=1)
-    top_k, throughput_ratio_k, correct = np.zeros(100), np.zeros(100), 0
+    predictions = np.argsort(model.predict(validation_lidar_data), axis=1)
+    correct, top_k, throughput_ratio_k = 0, [], []
+    best_throughput = np.sum(np.log2(np.max(validation_beam_output, axis=1) + 1))
     for pos in range(100):
         correct += np.sum(predictions[:, -1-pos] == np.argmax(validation_beam_output, axis=1))
-        top_k[pos] = correct / validation_beam_output.shape[0]
-        throughput_ratio_k[pos] = np.sum(np.log2(np.max(np.take_along_axis(beam_output_true, predictions, axis=1)[:, -1-pos:], axis=1) + 1)) / np.sum(np.log2(np.max(beam_output_true, axis=1) + 1))
+        top_k.append(correct / validation_beam_output.shape[0])
+        throughput_ratio_k.append(np.sum(np.log2(np.max(np.take_along_axis(
+            validation_beam_output, predictions, axis=1)[:, -1-pos:], axis=1) + 1)) / best_throughput)
     return correct, top_k, throughput_ratio_k
