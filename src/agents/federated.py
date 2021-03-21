e0bd89333867eced3d5ec95c1c596f40e6e1ead8
Mark Towers <mark.m.towers@gmail.com>
2021-03-21 23:37:24 +0000

    Fixed agents data folder locations

--- src/agents/federated.py
+++ src/agents/federated.py
@@ -13,8 +13,8 @@ from core.common import lidar_to_2d, get_beam_output, model_top_metric_eval
 
 
 def get_vehicle_dataset(total_vehicles, vehicle):
-    lidar_data = np.transpose(np.expand_dims(lidar_to_2d('data/lidar_train.npz'), 1), (0, 2, 3, 1))
-    beam_output, _ = get_beam_output('data/beams_output_train.npz')
+    lidar_data = np.transpose(np.expand_dims(lidar_to_2d('../data/lidar_train.npz'), 1), (0, 2, 3, 1))
+    beam_output, _ = get_beam_output('../data/beams_output_train.npz')
 
     # Split lidar data and beam labels
     split_lidar_data = lidar_data[vehicle * int(lidar_data.shape[0] / total_vehicles):(vehicle + 1) * int(lidar_data.shape[0] / total_vehicles), :, :, :]
@@ -26,8 +26,8 @@ def get_vehicle_dataset(total_vehicles, vehicle):
 
 
 def get_validation_dataset():
-    validation_lidar_data = np.transpose(np.expand_dims(lidar_to_2d('data/lidar_validation.npz'), 1), (0, 2, 3, 1))
-    validation_beam_output, _ = get_beam_output('data/beams_output_validation.npz')
+    validation_lidar_data = np.transpose(np.expand_dims(lidar_to_2d('../data/lidar_validation.npz'), 1), (0, 2, 3, 1))
+    validation_beam_output, _ = get_beam_output('../data/beams_output_validation.npz')
     dataset_test = tf.data.Dataset.from_tensor_slices((list(validation_lidar_data.astype(np.float32)),
                                                        list(validation_beam_output.astype(np.float32))))
     return dataset_test
@@ -41,7 +41,7 @@ def preprocess(dataset):
 
 def model_fn():
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Input(shape=(20, 200, 1)),
+        tf.keras.layers.InputLayer((20, 200, 1)),
         tf.keras.layers.Conv2D(5, 3, 1, padding='same'),  # kernel_initializer=initializers.HeUniform),
         tf.keras.layers.BatchNormalization(axis=3),
         tf.keras.layers.PReLU(shared_axes=[1, 2]),
@@ -79,18 +79,18 @@ def model_fn():
 num_vehicles, aggregation_rounds = 2, 30
 
 # initialise the federated learning
-federated_process = tff.learning.build_federated_averaging_process(
+federated_trainer = tff.learning.build_federated_averaging_process(
     model_fn,
     client_optimizer_fn=lambda: tf.keras.optimizers.Adam(lr=5e-3),
     server_optimizer_fn=lambda: tf.keras.optimizers.SGD(lr=.25))
 federated_evaluation = tff.learning.build_federated_evaluation(model_fn)
-federated_state = federated_process.initialize()
+federated_state = federated_trainer.initialize()
 
 # Generate vehicle training dataset and the testing dataset
 vehicle_training_dataset = [preprocess(get_vehicle_dataset(num_vehicles, v)) for v in range(num_vehicles)]
 validation_data = [preprocess(get_validation_dataset())]
-custom_val_lidar = np.transpose(np.expand_dims(lidar_to_2d('data/lidar_validation.npz'), 1), (0, 2, 3, 1))
-custom_val_beam, _ = get_beam_output('data/beams_output_validation.npz')
+custom_val_lidar = np.transpose(np.expand_dims(lidar_to_2d('../data/lidar_validation.npz'), 1), (0, 2, 3, 1))
+custom_val_beam, _ = get_beam_output('../data/beams_output_validation.npz')
 
 # Evaluation metrics
 metrics = {'training-top-1': [], 'training-top-10': [], 'testing-top-1': [], 'testing-top-10': [],
@@ -98,7 +98,7 @@ metrics = {'training-top-1': [], 'training-top-10': [], 'testing-top-1': [], 'te
 
 # Federated Training
 for round_num in range(aggregation_rounds):
-    federated_state, training_metrics = federated_process.next(federated_state, vehicle_training_dataset)
+    federated_state, training_metrics = federated_trainer.next(federated_state, vehicle_training_dataset)
     testing_metrics = federated_evaluation(federated_state.model, validation_data)
 
     # Save metrics
@@ -115,6 +115,6 @@ for round_num in range(aggregation_rounds):
     metrics['throughput-ratio-k'].append(throughput_ratio)
 
 # Save the agent results
-with open('federated-agent-metrics.json') as file:
+with open('../federated-agent-metrics.json') as file:
     json.dump(metrics, file)
 federated_state.model.save('models/federated-model')
