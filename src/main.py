ee82241b7b53db93d3e73c7b7d9adcc1ecb98df7
Mark Towers <mark.m.towers@gmail.com>
2021-04-11 16:33:36 +0100

    Added more model choices

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+
 import argparse
+import gc
 
 from agents.centralised import centralised_training
 from agents.distributed import distributed_training
@@ -9,18 +11,22 @@ from core.common import parse_model
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--agent', default='centralised', choices=['centralised', 'distributed', 'federated',
                                                                      'southampton'])
-parser.add_argument('-m', '--model', default='imperial', choices=['imperial', 'beamsoup-lidar',
-                                                                  'beamsoup-coord', 'beamsoup'])
+model_choices = ['imperial', 'beamsoup-coord', 'beamsoup-lidar', 'beamsoup-joint',
+                 'husky-coord', 'husky-lidar', 'husky-image', 'husky-fusion',
+                 'baseline-coord', 'baseline-lidar', 'baseline-image', 'baseline-fusion']
+parser.add_argument('-m', '--model', default='imperial', choices=model_choices)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    model_name, model, *train_validation_data = parse_model(parser)
+    print(f'Agents: {args.agent}, Model: {args.model}')
+    model, *train_validation_data = parse_model(args.model)
+    gc.collect()
 
     if args.agent == 'centralised':
-        centralised_training(model_name, model, *train_validation_data)
+        centralised_training(args.model, model, *train_validation_data)
     elif args.agent == 'distributed':
-        distributed_training(model_name, model, *train_validation_data)
+        distributed_training(args.model, model, *train_validation_data)
     elif args.agent == 'federated':
         federated_training()
     elif args.agent == 'southampton':
