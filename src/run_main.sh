01b5d13c372019cecdb104714b1fdd0a31340396
StringTheory <mark.m.towers@gmail.com>
2021-04-25 19:23:59 +0100

    Added run file for the use on the supercomputer

---
+++ src/run_main.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+
+#SBATCH --partition=gtx1080
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+
+module load conda
+# conda create -n 5GBeamAlignment python=3.6
+# pip install tensorflow
+conda activate 5GBeamAlignment
+
+module load cuda/11.0
+
+echo $PWD
+cd ~/5G-Beam-Selection/src/
+
+python main.py --agent centralised --model imperial
+
+python main.py --agent centralised --model beamsoup-lidar
+python main.py --agent centralised --model beamsoup-coord
+python main.py --agent centralised --model beamsoup-joint
+
+python main.py --agent centralised --model husky-fusion
+
+python main.py --agent centralised --model southampton
+
+python main.py --agent southampton --model imperial
+python main.py --agent southampton --model southampton
+
+python main.py --agent federated --model imperial
+python main.py --agent federated --model beamsoup-coord
+python main.py --agent distributed --model imperial
+python main.py --agent distributed --model beamsoup-coord
