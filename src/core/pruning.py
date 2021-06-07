e1577b3b3c03e4a36952e56c11180623e5081348
StringTheory <mark.m.towers@gmail.com>
2021-06-07 20:59:17 +0100

    Added to_sparse and to_dense for numpy matrices

--- src/core/pruning.py
+++ src/core/pruning.py
@@ -38,7 +38,7 @@ def prune_layer(model_weights, desired_sparsity):
     return np.reshape(np.vectorize(mask)(model_weights), shape)
 
 
-def prune_model(model, desired_sparsity):
+def prune_model(model, desired_sparsity, clone_model=True):
     def apply_pruning_to_dense(layer):
         if isinstance(layer, tf.keras.layers.Dense):
             init = tf.constant_initializer(prune_layer(layer.weights[0].numpy(), desired_sparsity))
@@ -47,7 +47,41 @@ def prune_model(model, desired_sparsity):
                                           bias_initializer=bias_initializer)
         return layer
 
-    return tf.keras.models.clone_model(model, clone_function=apply_pruning_to_dense)
+    if clone_model:
+        return tf.keras.models.clone_model(model, clone_function=apply_pruning_to_dense)
+    else:
+        return [apply_pruning_to_dense(layer) for layer in model.layers]
+
+
+def to_sparse(weights, dtype=np.float32):
+    indexed = list(np.ndenumerate(weights))
+    sparse_matrix = []
+
+    def is_zero(elem):
+        ind, val = elem
+        if val != 0:
+            sparse_matrix.append(np.array([float(ind[0]), float(ind[1]), val]))
+
+    sparsify = np.vectorize(is_zero, signature='(0)->()')
+    sparsify(indexed)
+
+    return np.array(sparse_matrix, dtype)
+
+
+def to_dense(sparse_matrix, dtype=np.float32):
+    x = int(np.max(sparse_matrix[:, 1])) + 1
+    y = int(sparse_matrix[-1][0]) + 1
+
+    base = np.zeros((x, y), dtype)
+
+    def setter(elem):
+        x_ind, y_ind, val = elem
+        base[int(x_ind)][int(y_ind)] = val
+
+    array_setter = np.vectorize(setter, signature='(0)->()')
+    array_setter(sparse_matrix)
+
+    return base
 
 
 def get_gzipped_model_size(model):
